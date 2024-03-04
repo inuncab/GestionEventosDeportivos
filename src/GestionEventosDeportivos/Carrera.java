3381086a08ccebb114049d4946ed63c2738d5db5
unknown <vchacal327@g.educaand.es>
2024-03-04 21:37:38 +0100

    Cambios en metodos de Carrera - vchacal327

--- src/GestionEventosDeportivos/Carrera.java
+++ src/GestionEventosDeportivos/Carrera.java
@@ -17,18 +17,27 @@ public class Carrera extends EventoDeportivo {
     @Override
     public ArrayList<Participante> obtenerGanador() {
         double tiempoMinimo = Double.MAX_VALUE;
-        ParticipanteCarrera participanteMin = null;
+        Participante participanteMin = null;
 
         for (int i = 0; i < participantes.size(); i++) {
 
-            double tiempo = participantes.get(i).getTiempo();
+            // Verificamos si el participante es de tipo ParticipanteCarrera
+            if (participanteMin instanceof ParticipanteCarrera) {
+                // Si es de tipo participanteCarrera lo guardamos como un participanteCarrera
+                ParticipanteCarrera participanteCarrera = (ParticipanteCarrera) participanteMin;
 
-            if (tiempo < tiempoMinimo) {
-                tiempoMinimo = tiempo;
-                participanteMin = participantes.get(i);
+                double tiempo = participanteCarrera.getTiempo();
+
+                if (tiempo < tiempoMinimo) {
+                    tiempoMinimo = tiempo;
+                    participanteMin = participantes.get(i);
+                }
             }
-        }
 
+
+
+
+        }
         ArrayList<Participante> ganadores = new ArrayList<>();
         if (participanteMin != null) {
             ganadores.add(participanteMin);
