30eb0b6ffc82869775ccd0baceb756d2b89fbb16
inuncab <inuncab924@g.educaand.es>
2024-03-04 19:07:25 +0100

    prueba

--- src/GestionEventosDeportivos/Carrera.java
+++ src/GestionEventosDeportivos/Carrera.java
@@ -1,2 +1,14 @@
-package GestionEventosDeportivos;public class Carrera {
+package GestionEventosDeportivos;
+
+import java.time.LocalDateTime;
+
+public class Carrera extends EventoDeportivo{
+    public Carrera(String nombre, LocalDateTime fecha, String lugar) {
+        super(nombre, fecha, lugar);
+    }
+
+    @Override
+    public Participante obtenerGanador() {
+        return null;
+    }
 }
